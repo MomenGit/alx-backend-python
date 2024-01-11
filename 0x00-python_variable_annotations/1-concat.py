d7b7785a94e50126d182442f216e867708497e95
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-11 14:08:12 +0200

    Write 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates str1 and str2 and returns a new string"""
+    return str1 + str2
