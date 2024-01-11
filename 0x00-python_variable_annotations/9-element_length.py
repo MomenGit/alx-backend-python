77844c856758d67dea1f70468d3c06cc24d2ad3c
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-11 16:28:39 +0200

    Add function doc

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Defines function element_length"""
+
 from typing import Iterable, Sequence, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """"""
+    """Returns a list of tuples containing a sequence and its length"""
     return [(i, len(i)) for i in lst]
