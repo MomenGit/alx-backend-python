8354c692493da881a1f82b85d46f7a2bd7d0318b
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-11 16:12:43 +0200

    Write 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Defines function sum_mixed_list"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Returns the sum of the input mixed list of float or int numbers"""
+
+    sum: float = 0.0
+
+    for num in mxd_lst:
+        sum += num
+
+    return sum
