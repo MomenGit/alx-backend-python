e6fecbe7d1e53054d519013074f1da7f8f2ddfde
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-11 16:17:30 +0200

    Fix type annotations

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,10 @@
 """Defines function sum_list"""
 
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of the input list of float numbers"""
 
     sum: float = 0.0
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of the input mixed list of float or int numbers"""
 
     sum: float = 0.0
