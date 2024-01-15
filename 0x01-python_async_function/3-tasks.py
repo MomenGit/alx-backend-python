687f972b332b68ab03022231dd03911ef0ba6ce8
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-15 21:28:54 +0200

    Write 3-tasks.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Defines wait_n function"""
+import asyncio
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """
+    Return Task wait_random) which
+    waits for a random delays between 0 and max_delay
+    (included and float value) seconds and eventually returns it
+    Args:
+        max_delay(int): The max number for the random value generated for delay
+    """
+
+    return asyncio.Task(wait_random(max_delay))
