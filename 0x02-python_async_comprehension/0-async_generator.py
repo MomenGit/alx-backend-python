7f8e360acee08e36b76da1f3adc65b19826e5c59
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-16 12:11:09 +0200

    Change var i to _

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,6 +9,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """Loops 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.random()*10
