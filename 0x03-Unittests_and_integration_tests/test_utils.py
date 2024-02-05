cda2d3109fb846a7de75636fa6d125235c2c6ba8
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-05 23:26:56 +0200

    Add get_json function mock tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""Test cases for the Utils module"""
 from typing import Mapping, Sequence
 import utils
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,3 +29,21 @@ class TestAccessNestedMap(unittest.TestCase):
         utils.access_nested_map function
         """
         self.assertRaises(KeyError)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test cases for utils.get_json function"""
+
+    @parameterized.expand(
+        [("http://example.com", {"payload": True}),
+         ("http://holberton.io", {"payload": False})])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Mock testing for utils.get_json function"""
+        mock_response = MagicMock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = utils.get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
