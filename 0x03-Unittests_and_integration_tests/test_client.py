ddaa9626ae88744ff7bad9e4baee46f61c78be92
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-09 18:26:15 +0200

    Write test cases for client.GithubOrgClient.org method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Test cases for client module"""
+import unittest
+from unittest.mock import MagicMock, patch
+import client
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test cases for client.GithubOrgClient class"""
+
+    @parameterized.expand([
+        ('google', {"payload": True}),
+        ('abc', {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_org(self, org, expected, mocked_get):
+        """Test cases for client.GithubOrgClient.org function"""
+        mock_response = MagicMock()
+        mock_response.json.return_value = expected
+        mocked_get.return_value = mock_response
+
+        org_client = client.GithubOrgClient(org)
+        org1 = org_client.org
+        org2 = org_client.org
+        mocked_get.assert_called_once()
+        self.assertEqual(org1, org2)
+
+
+if __name__ == "__main__":
+    unittest.main()
